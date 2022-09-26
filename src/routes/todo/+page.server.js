fb2528805dd3d5695794eb423c5cfbc4733fcdd6
Joe Irvine <jirvine124@gmail.com>
2022-09-25 21:41:56 -0700

    small updates

---
+++ src/routes/todo/+page.server.js
@@ -0,0 +1,31 @@
+import db from '$lib/mongo';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load() {
+	let tasks = await db.collection('Tasks').find({}).toArray();
+	let parsedTasks = JSON.parse(JSON.stringify(tasks));
+	return {
+		parsedTasks
+	};
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	done: () => {
+		console.log('done');
+	},
+	delete: async ({ request }) => {
+		const data = await request.formData();
+		const name = data.get('id');
+		const index = data.get('index');
+		console.log(`Delete see ${name}`);
+		db.collection('Tasks').deleteOne({ name: name });
+	},
+	add: async ({ request }) => {
+		const data = await request.formData();
+		const todo = data.get('todo');
+
+		console.log(`You need to do ${todo}`);
+		db.collection('Tasks').insertOne({ name: todo, status: 'not done' });
+	}
+};
---
+++ src/routes/todo/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { page } from '$app/stores';
+	import { enhance } from '$app/forms';
+
+	console.log($page.data);
+</script>
+
+<h1>Tasks</h1>
+<div>
+	<form method="POST" action="?/add" use:enhance>
+		<input id="new-task" name="todo" type="text" placeholder="New Task" />
+		<button>Add</button>
+		{#each $page.data.parsedTasks as task, index}
+			<div>
+				<form class="task" method="POST" action="?/delete" use:enhance>
+					<p>{task.name}</p>
+					<input hidden name="id" type="text" value={task.name} />
+					<input hidden name="index" type="text" value={index} />
+					<button>delete</button>
+				</form>
+			</div>
+		{/each}
+	</form>
+</div>
+
+<style>
+	.task {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		width: 500px;
+		height: 45px;
+		background-color: lightblue;
+		margin-bottom: 2px;
+	}
+</style>
